f085fb27e34985e572ce662d1a21732330241dca
ahmedtechm <ahmedtechm2050@gmail.com>
2023-03-08 12:33:37 +0400

    Added Application Main Menu

--- src/InvoicingSystemDB/ShopMainClassDB.java
+++ src/InvoicingSystemDB/ShopMainClassDB.java
@@ -3,6 +3,20 @@ package InvoicingSystemDB;
 public class ShopMainClassDB {
 
 	public static void main(String[] args) {
+		
+		
+    	// Application Main Menu 
+		
+    	System.out.println("⫸⫸⫸⫸⫸⫸⫸⫸👉 Welcome to Groceries Shop System 👈⫷⫷⫷⫷⫷⫷⫷⫷");
+        System.out.println("\n1- Shop Settings");
+        System.out.println("2- Manage Shop Items");
+        System.out.println("3- Create New Invoice");
+        System.out.println("4- Report: Statistics");
+        System.out.println("5- Report: All Invoices");
+        System.out.println("6- Search (1) Invoice");
+        System.out.println("7- Program Statistics");
+        System.out.println("8- Exit");
+        System.out.print("Enter your choice: ");
 
 	}
 
