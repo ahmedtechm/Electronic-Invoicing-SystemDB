648a05373cbb7902317204650ccc6878bd9dc303
ahmedtechm <ahmedtechm2050@gmail.com>
2023-03-16 14:23:53 +0400

    Added Java code for inserting Invoice data into a SQL Server database table

--- src/InvoicingSystemDB/ShopMainClassDB.java
+++ src/InvoicingSystemDB/ShopMainClassDB.java
@@ -22,14 +22,13 @@ public class ShopMainClassDB {
 		Scanner scan = new Scanner(System.in).useDelimiter("\n");
 		Scanner scanner = new Scanner(System.in);
 
-		
 		System.out.println("Enter User ID : ");
 		String userID = scanner.nextLine();
 		System.out.println("Enter User password : ");
 		String passID = scanner.nextLine();
-	
+
 		Connection connection = null;
-		
+
 		boolean exit = false;
 
 		InvoiceDB invoiceobj = new InvoiceDB();
@@ -64,25 +63,25 @@ public class ShopMainClassDB {
 					String Item = "CREATE TABLE Item (" + "itemID INT PRIMARY KEY, " + " itemName VARCHAR(255), "
 							+ " itemPrice FLOAT , " + "itemQuantity INT, " + "qtyAmountPrice FLOAT  " + ")";
 
-					//statement.executeUpdate(Item);
+					// statement.executeUpdate(Item);
 
 					String Shop = "CREATE TABLE Shop (" + " telephone INT, " + " fax VARCHAR(255), "
 							+ "email VARCHAR(255), " + "webSite VARCHAR(255), " + "shopName VARCHAR(255) " + ")";
 
-					//statement.executeUpdate(Shop);
+					// statement.executeUpdate(Shop);
 
 					String Invoice = "CREATE TABLE Invoice (" + " invoiceDate int, "
 							+ " customerFullName VARCHAR(255), " + "phoneNumber INT, " + "numberOfItem INT, "
 							+ "totalAmount FLOAT , " + "paidAmount FLOAT , " + "balance FLOAT " + ")";
 
-				//statement.executeUpdate(Invoice);
+					// statement.executeUpdate(Invoice);
 
 					System.out.println("\nexecute completed ");
 
 				} catch (Exception ex) {
 					System.err.println(ex);
 				}
-			
+
 			case 1:
 				// Shop Settings Menu
 				System.out.println("\n1. Load Data (Items and invoices)");
@@ -110,60 +109,56 @@ public class ShopMainClassDB {
 
 					break;
 				case 3:
-               	 // Set Invoice Header
+					// Set Invoice Header
 
 					try {
-                    String shopName1 = invoiceobj.shopobj.getShopName();
-					
-					System.out.print("Enter telephone number: ");
-					Integer telephone = scanner.nextInt();
-					invoiceobj.shopobj.setTelephone(telephone);
+						String shopName1 = invoiceobj.shopobj.getShopName();
 
-					System.out.print("Enter fax number: ");
-					String fax = scanner.next();
-					invoiceobj.shopobj.setFax(fax);
+						System.out.print("Enter telephone number: ");
+						Integer telephone = scanner.nextInt();
+						invoiceobj.shopobj.setTelephone(telephone);
 
-					System.out.print("Enter new email Id: ");
-					String email = scanner.next();
-					invoiceobj.shopobj.setEmail(email);
+						System.out.print("Enter fax number: ");
+						String fax = scanner.next();
+						invoiceobj.shopobj.setFax(fax);
 
-					System.out.print("Enter webSite: ");
-					String webSite = scanner.next();
-					invoiceobj.shopobj.setWebSite(webSite);
+						System.out.print("Enter new email Id: ");
+						String email = scanner.next();
+						invoiceobj.shopobj.setEmail(email);
 
-					System.out.println("Invoice telephone saved : " + telephone);
-					System.out.println("Invoice fax saved : " + fax);
-					System.out.println("Invoice email saved : " + email);
-					System.out.println("Invoice webSite saved: " + webSite);
-					
-					
-				
-					Driver driver = (Driver) Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver").newInstance();
-		        	DriverManager.registerDriver(driver);
-		        	connection = DriverManager.getConnection(url, userID, passID);
-		        	 Statement statement = connection.createStatement();
-		        
-					
-					
-				
-					String sql = "INSERT INTO Shop (shopName, telephone, fax, email, webSite) " +
-		
-					"VALUES ("+"'"+shopName1+"','"+telephone+"','"+fax+"','"+email+"','"+webSite+ "')";
-   
-					  statement.executeUpdate(sql);
-					  
-					  System.out.println("saved successfully...");
-		            
-				}catch (Exception e) {
-					// TODO: handle exception
-				}
-				
-				
+						System.out.print("Enter webSite: ");
+						String webSite = scanner.next();
+						invoiceobj.shopobj.setWebSite(webSite);
+
+						System.out.println("Invoice telephone saved : " + telephone);
+						System.out.println("Invoice fax saved : " + fax);
+						System.out.println("Invoice email saved : " + email);
+						System.out.println("Invoice webSite saved: " + webSite);
+
+						Driver driver = (Driver) Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver")
+								.newInstance();
+						DriverManager.registerDriver(driver);
+						connection = DriverManager.getConnection(url, userID, passID);
+						Statement statement = connection.createStatement();
+
+						String sql = "INSERT INTO Shop (shopName, telephone, fax, email, webSite) " +
+
+								"VALUES (" + "'" + shopName1 + "','" + telephone + "','" + fax + "','" + email + "','"
+								+ webSite + "')";
+
+						statement.executeUpdate(sql);
+
+						System.out.println("saved successfully...");
+
+					} catch (Exception e) {
+						// TODO: handle exception
+					}
+
 					break;
 				case 4:
 					break;
 				default:
-					  System.out.println("Invalid choice. Please enter a valid number.");
+					System.out.println("Invalid choice. Please enter a valid number.");
 				}
 				break;
 			case 2:
@@ -180,49 +175,50 @@ public class ShopMainClassDB {
 				case 1:
 					// Add Items
 					try {
-					ItemDB itemobj = new ItemDB();
+						ItemDB itemobj = new ItemDB();
 
-					System.out.print("Enter item ID: ");
-					Integer itemID = scanner.nextInt();
-					invoiceobj.shopobj.itemobj.setItemID(itemID);
+						System.out.print("Enter item ID: ");
+						Integer itemID = scanner.nextInt();
+						invoiceobj.shopobj.itemobj.setItemID(itemID);
 
-					System.out.print("Enter item name: ");
-					String itemName = scan.next();
-					invoiceobj.shopobj.itemobj.setItemName(itemName);
+						System.out.print("Enter item name: ");
+						String itemName = scan.next();
+						invoiceobj.shopobj.itemobj.setItemName(itemName);
 
-					System.out.print("Enter item price: ");
-					float itemPrice = scanner.nextFloat();
-					invoiceobj.shopobj.itemobj.setItemPrice(itemPrice);
+						System.out.print("Enter item price: ");
+						float itemPrice = scanner.nextFloat();
+						invoiceobj.shopobj.itemobj.setItemPrice(itemPrice);
 
-					System.out.print("Enter Item Quantity: ");
-					Integer itemQuantity = scanner.nextInt();
-					invoiceobj.shopobj.itemobj.setItemQuantity(itemQuantity);
+						System.out.print("Enter Item Quantity: ");
+						Integer itemQuantity = scanner.nextInt();
+						invoiceobj.shopobj.itemobj.setItemQuantity(itemQuantity);
 
-					System.out.print("Enter qty Amount Price: ");
-					float qtyAmountPrice = scanner.nextFloat();
-					invoiceobj.shopobj.itemobj.setQtyAmountPrice(qtyAmountPrice);
+						System.out.print("Enter qty Amount Price: ");
+						float qtyAmountPrice = scanner.nextFloat();
+						invoiceobj.shopobj.itemobj.setQtyAmountPrice(qtyAmountPrice);
 
-					itemList.add(itemobj);
-					
-					System.out.println("item ID saved : " + itemID);
-					System.out.println("item name saved : " + itemName);
-					System.out.println("item price saved : " + itemPrice);
-					System.out.println("Item Quantity saved: " + itemQuantity);
-					System.out.println("Item qty Amount Price saved: " + qtyAmountPrice);
-					
-					Driver driver = (Driver) Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver").newInstance();
-		        	DriverManager.registerDriver(driver);
-		        	connection = DriverManager.getConnection(url, userID, passID);
-		        	 Statement statement = connection.createStatement();
-		        
+						itemList.add(itemobj);
 
-					String sql1= "INSERT INTO Item (itemID, itemName, itemPrice, itemQuantity, qtyAmountPrice) "+ 
-							"VALUES('" + itemID + "','" + itemName + "','" + itemPrice + "','" + itemQuantity + "','" + qtyAmountPrice + "')";
-				
-					  statement.executeUpdate(sql1);
-					  
-					  System.out.println("saved successfully...");
-					}catch (Exception e) {
+						System.out.println("item ID saved : " + itemID);
+						System.out.println("item name saved : " + itemName);
+						System.out.println("item price saved : " + itemPrice);
+						System.out.println("Item Quantity saved: " + itemQuantity);
+						System.out.println("Item qty Amount Price saved: " + qtyAmountPrice);
+
+						Driver driver = (Driver) Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver")
+								.newInstance();
+						DriverManager.registerDriver(driver);
+						connection = DriverManager.getConnection(url, userID, passID);
+						Statement statement = connection.createStatement();
+
+						String sql1 = "INSERT INTO Item (itemID, itemName, itemPrice, itemQuantity, qtyAmountPrice) "
+								+ "VALUES('" + itemID + "','" + itemName + "','" + itemPrice + "','" + itemQuantity
+								+ "','" + qtyAmountPrice + "')";
+
+						statement.executeUpdate(sql1);
+
+						System.out.println("saved successfully...");
+					} catch (Exception e) {
 						// TODO: handle exception
 					}
 
@@ -247,34 +243,61 @@ public class ShopMainClassDB {
 				break;
 			case 3:
 				// Create New Invoice
+				try {
 
-				System.out.print("Enter Customer Full Name : ");
-				String customerFullName = scan.next();
-				invoiceobj.setCustomerFullName(customerFullName);
+					System.out.print("\nEnter Customer Full Name : ");
+					String customerFullName = scan.next();
+					invoiceobj.setCustomerFullName(customerFullName);
 
-				System.out.print("Enter Customer Phone Number : ");
-				Integer phoneNumber = scanner.nextInt();
-				invoiceobj.setPhoneNumber(phoneNumber);
+					System.out.print("\nEnter Customer Phone Number : ");
+					Integer phoneNumber = scanner.nextInt();
+					invoiceobj.setPhoneNumber(phoneNumber);
 
-				System.out.print("Enter Invoice Date : ");
-				Integer invoiceDate = scanner.nextInt();
-				invoiceobj.setInvoiceDate(invoiceDate);
+					System.out.print("\nEnter Invoice Date : ");
+					Integer invoiceDate = scanner.nextInt();
+					invoiceobj.setInvoiceDate(invoiceDate);
 
-				System.out.print("Enter Number of Item : ");
-				Integer numberOfItem = scanner.nextInt();
-				invoiceobj.setNumberOfItem(numberOfItem);
+					System.out.print("\nEnter Number of Item : ");
+					Integer numberOfItem = scanner.nextInt();
+					invoiceobj.setNumberOfItem(numberOfItem);
 
-				System.out.print("Enter Total Amount : ");
-				float totalAmount = scanner.nextFloat();
-				invoiceobj.setTotalAmount(totalAmount);
+					System.out.print("\nEnter Total Amount : ");
+					float totalAmount = scanner.nextFloat();
+					invoiceobj.setTotalAmount(totalAmount);
 
-				System.out.print("Enter Paid Amount : ");
-				float paidAmount = scanner.nextFloat();
-				invoiceobj.setPaidAmount(paidAmount);
+					System.out.print("\nEnter Paid Amount : ");
+					float paidAmount = scanner.nextFloat();
+					invoiceobj.setPaidAmount(paidAmount);
 
-				System.out.print("Enter Balance : ");
-				float balance = scanner.nextFloat();
-				invoiceobj.setBalance(balance);
+					System.out.print("\nEnter Balance : ");
+					float balance = scanner.nextFloat();
+					invoiceobj.setBalance(balance);
+
+					System.out.println("Customer Full Name saved : " + customerFullName);
+					System.out.println("Customer Phone Number saved : " + phoneNumber);
+					System.out.println("Invoice Date saved : " + invoiceDate);
+					System.out.println("Number of Item saved: " + numberOfItem);
+					System.out.println("Total Amount saved: " + totalAmount);
+					System.out.println("Paid Amount saved : " + paidAmount);
+					System.out.println("Balance saved : " + balance);
+
+					
+					Driver driver = (Driver) Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver")
+							.newInstance();
+					DriverManager.registerDriver(driver);
+					connection = DriverManager.getConnection(url, userID, passID);
+					Statement statement = connection.createStatement();
+
+					String sqlInvoice = "INSERT INTO Invoice (invoiceDate, customerFullName, phoneNumber, numberOfItem, totalAmount, paidAmount, balance)"
+							+ "VALUES ('" + invoiceDate + "','" + customerFullName + "','" + phoneNumber + "','"
+							+ numberOfItem + "','" + totalAmount + paidAmount + "','" + balance + "')";
+					
+					statement.executeUpdate(sqlInvoice);
+
+					System.out.println("saved successfully...");
+				} catch (Exception e) {
+					// TODO: handle exception
+				}
 
 				break;
 			case 4:
