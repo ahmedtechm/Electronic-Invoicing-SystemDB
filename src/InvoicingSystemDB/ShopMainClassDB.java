c1e0fb6cb2e92cb1a90d4e41887098cbedc50b56
ahmedtechm <ahmedtechm2050@gmail.com>
2023-03-22 10:36:50 +0400

    Updated database connection code to improve security

--- src/InvoicingSystemDB/ShopMainClassDB.java
+++ src/InvoicingSystemDB/ShopMainClassDB.java
@@ -17,21 +17,32 @@ public class ShopMainClassDB {
 		String url = "jdbc:sqlserver://localhost:1433;" + "databaseName=InvoicingDB;" + "encrypt=true;"
 				+ "trustServerCertificate=true";
 
-		String user = "sa";
-		String pass = "root";
+		String userID = "sa";
+		String passID = "root";
 
 		ArrayList<ItemDB> itemList = new ArrayList<ItemDB>();
 
 		Scanner scan = new Scanner(System.in).useDelimiter("\n");
 		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Connecting to database...");
 
-		System.out.println("Enter User ID : ");
-		String userID = scanner.nextLine();
-		System.out.println("Enter User password : ");
-		String passID = scanner.nextLine();
+		boolean passwordCondition = true;
+		while (passwordCondition) {
+			System.out.println("\nEnter User ID: ");
+			userID = scanner.nextLine();
+			System.out.println("Enter User password: ");
+			passID = scanner.nextLine();
+				
+			if (userID.equals("sa") && passID.equals("root")) {
+				passwordCondition = false;
+			} else {
+				System.out.println("Wrong username and password");
+			}
+		}
 
 		Connection connection = null;
-
+		
 		boolean exit = false;
 
 		InvoiceDB invoiceobj = new InvoiceDB();
